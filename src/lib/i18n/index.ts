4abbee1481e8bca326965276fdc36a441e78c406
jules <jules.thibault2002@gmail.com>
2022-01-06 20:43:21 +0100

    🌐 add i18n config

---
+++ src/lib/i18n/index.ts
@@ -0,0 +1,31 @@
+import { t, addMessages, getLocaleFromNavigator, init, locale, locales } from 'svelte-i18n';
+import en from './en.json';
+import frFR from './fr-FR.json';
+
+export type Language = 'en' | 'fr-FR';
+type LocaleJson = typeof en;
+type Locales = { [key in Language]: LocaleJson };
+
+const buybackLocales: Locales = {
+	en: en,
+	'fr-FR': frFR
+};
+
+let i18nLoaded = false;
+
+/**
+ * Initialize locale dictionnaries and use navigator locale.
+ */
+const initI18n = () => {
+	if (!i18nLoaded) {
+		for (const [key, value] of Object.entries(buybackLocales)) addMessages(key, value);
+
+		init({
+			fallbackLocale: 'en',
+			initialLocale: getLocaleFromNavigator()
+		});
+		i18nLoaded = true;
+	}
+};
+
+export { t, initI18n, locale, locales };
