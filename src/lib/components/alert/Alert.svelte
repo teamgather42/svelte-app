40514323da913a75f07068ef1adfe11e76a2f02c
jules <jules.thibault2002@gmail.com>
2022-01-07 12:26:19 +0100

    ✨ add <Alert>

---
+++ src/lib/components/alert/Alert.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	type Variant = 'danger' | 'success';
+
+	/**
+	 * The variant of the alert.
+	 */
+	export let variant: Variant = 'danger';
+	/**
+	 * If the alert should be shown or not.
+	 */
+	export let hide: boolean = true;
+
+	$: componentClass =
+		(variant === 'danger' && 'bg-red-100 border border-red-500 text-red-500') ||
+		(variant === 'success' && 'bg-green-100 border border-green-500 text-green-500');
+
+	const hideAlert = () => (hide = !hide);
+</script>
+
+{#if !hide}
+	<section role="alert" class={`flex rounded-md w-fit py-2 px-4 ml-4 ${componentClass}`}>
+		<p class="text-base font-light"><slot /></p>
+		<button on:click={hideAlert} role="button" class="ml-5"><i class="fas fa-times" /></button>
+	</section>
+{/if}
