16a6c9c089d5fdec365e5ef7baa1755f38a2df55
jules <jules.thibault@decathlon.com>
2022-01-08 00:24:58 +0100

    ✨ add <TextInput>

---
+++ src/lib/components/inputs/TextInput.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { cn } from '$lib/utils/classnames';
+
+	type Type = 'text' | 'email' | 'password' | 'search';
+
+	/**
+	 * Input type.
+	 */
+	export let type: Type = 'text';
+	/**
+	 * Input value.
+	 */
+	export let value: string = '';
+	/**
+	 * Input's placeholder.
+	 */
+	export let placeholder: string = null;
+	/**
+	 * Input's label.
+	 */
+	export let label: string = null;
+	/**
+	 * Input's id.
+	 */
+	export let id: string = null;
+	/**
+	 * Helper text under the input.
+	 */
+	export let helperText: string = '';
+	/**
+	 * Allow to show error message.
+	 */
+	export let error: boolean = false;
+	/**
+	 * Allow to show valid message.
+	 */
+	export let valid: boolean = false;
+
+	let element;
+
+	onMount(() => (element.type = type));
+
+	$: componentClass = cn(
+		!error && !valid && 'border-gray-300 focus:ring-blue-500',
+		error && 'border-red-500 focus:ring-red-500',
+		valid && 'border-green-500 focus:ring-green-500',
+		'appearance-none',
+		'focus:ring-1',
+		'outline-none',
+		'rounded',
+		'border',
+		'px-3',
+		'py-3'
+	);
+</script>
+
+<section class="flex flex-col w-fit">
+	{#if label}
+		<label class="text-sm font-bold" for={id}>{label}</label>
+	{/if}
+	<input bind:value on:input bind:this={element} {placeholder} {id} class={componentClass} />
+	{#if helperText}
+		<span class:text-red-500={error} class:text-green-500={valid} class="text-sm italic"
+			>{helperText}</span
+		>
+	{/if}
+</section>
