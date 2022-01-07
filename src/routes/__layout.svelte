883f45317d6e9f24bf913ddc8ff97489a636ea99
jules <jules.thibault@decathlon.com>
2022-01-07 18:52:43 +0100

    ✨ add fontawesome

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,9 @@
 	import '../base.css';
 </script>
 
+<svelte:head>
+	<!-- Font Awesome -->
+	<script src="https://kit.fontawesome.com/fc13e8f391.js" crossorigin="anonymous"></script>
+</svelte:head>
+
 <slot />
