919a49595fe000b4c23ef7a8588bec512b7020ce
Jules THIBAULT <jules.thibault2002@gmail.com>
2022-01-09 12:22:14 -0800

    🏷️ add new types to tailwind.types.ts

--- src/lib/types/tailwind.types.ts
+++ src/lib/types/tailwind.types.ts
@@ -1,5 +1,26 @@
 export type Rounded = 'rounded' | 'rounded-md' | 'rounded-lg' | 'rounded-full';
 
+export type Size =
+	| 'xs'
+	| 'sm'
+	| 'base'
+	| 'lg'
+	| 'xl'
+	| '2xl'
+	| '3xl'
+	| '4xl'
+	| '5xl'
+	| '6xl'
+	| '7xl'
+	| '8xl'
+	| '9xl';
+
+export type TextSize = `text-${Size}`;
+
+export type BackgroundColor = `bg-${Color}`;
+
+export type TextColor = `text-${Color}`;
+
 export type Color =
 	| 'black'
 	| 'white'
