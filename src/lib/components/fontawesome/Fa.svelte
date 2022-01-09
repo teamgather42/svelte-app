ae5b68049c0df43aa4e1edc1ee05cfb701628f3d
Jules THIBAULT <jules.thibault2002@gmail.com>
2022-01-09 13:35:06 -0800

    ✨ add <Fa>

---
+++ src/lib/components/fontawesome/Fa.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { IconPrefix, IconName, IconSize } from '$lib/types';
+	import { cn } from '$lib/utils/classnames';
+
+	/**
+	 * Base font awesome prefix.
+	 */
+	const basePrefix: string = 'fa';
+
+	/**
+	 * Fontawesome icon array with prefix and icon name.
+	 */
+	export let icon: [IconPrefix, IconName];
+	/**
+	 * Size of the icon.
+	 */
+	export let iconSize: IconSize = '2x';
+
+	$: componentClass = cn(`${icon[0]}`, `${basePrefix}-${icon[1]}`, `${basePrefix}-${iconSize}`);
+</script>
+
+<i class={componentClass} />
