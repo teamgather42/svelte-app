c73db4504517c44a5bae987a511100dbeba2bbe4
Jules THIBAULT <jules.thibault2002@gmail.com>
2022-01-09 14:40:13 -0800

    ✨ add env.variables.ts

---
+++ src/lib/utils/env.variables.ts
@@ -0,0 +1,7 @@
+const getBaseUrl = import.meta.env.BASE_URL;
+
+const getApiUrl = import.meta.env.API_BASE_URL;
+
+const getApiVersion = import.meta.env.API_VERSION;
+
+export { getBaseUrl, getApiUrl, getApiVersion };
