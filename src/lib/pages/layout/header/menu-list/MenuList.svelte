9eb445824a3b6b75f3c479ca3decc80ad3411f61
Jules THIBAULT <jules.thibault2002@gmail.com>
2022-01-09 16:10:48 -0800

    ✨ add <MenuList>

---
+++ src/lib/pages/layout/header/menu-list/MenuList.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	type Option = {
+		href: string;
+		label: string;
+	};
+
+	export let heading: string = '';
+	export let options: Option[] = null;
+</script>
+
+<section>
+	<h2 class="border-b text-xl">{heading}</h2>
+	<ul>
+		{#each options as { label, href }}
+			<li class="my-2 hover:bg-red-500"><a class="block py-2" {href}>{label}</a></li>
+		{/each}
+	</ul>
+</section>
