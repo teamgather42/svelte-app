913badf23fda8d0696de26edd358e44e8cba23f3
Julestblt <jules.thibault2002@gmail.com>
2022-01-10 20:06:39 +0100

    ✨ add <Footer> and usage

---
+++ src/lib/pages/layout/footer/Footer.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import Logo from '$lib/components/logo/Logo.svelte';
+	import Fa from '$lib/components/fontawesome/Fa.svelte';
+	import { LanguageSelector, CurrencySelector } from '$lib/components/selectors/';
+</script>
+
+<footer class="border-t-2 border-grey-500">
+	<section class="flex justify-center py-8">
+		<Logo class="font-archivo text-3xl" />
+	</section>
+	<nav>
+		<ul class="flex flex-col items-center">
+			<li><a href="/">Terms of Service.</a></li>
+			<li><a href="/">Security</a></li>
+			<li><a href="/">Privacy Policy</a></li>
+			<li><a href="/">Sitemap</a></li>
+		</ul>
+	</nav>
+	<div class="flex flex-col w-full md:flex-row md:justify-between items-center md:pb-5">
+		<section class="flex justify-around w-300">
+			<CurrencySelector />
+			<LanguageSelector />
+		</section>
+		<section class="social-links">
+			<ul class="flex justify-between w-full">
+				<li class="md:mr-4 lg:mr-10 xl:mr-20">
+					<a href="https://twitter.com" target="_blank">
+						<Fa icon={['fab', 'twitter']} iconSize="2x" />
+					</a>
+				</li>
+				<li class="md:mr-4 lg:mr-10 xl:mr-20">
+					<a href="https://facebook.com" target="_blank">
+						<Fa icon={['fab', 'facebook']} iconSize="2x" />
+					</a>
+				</li>
+				<li class="md:mr-4 lg:mr-10 xl:mr-20">
+					<a href="https://instagram.com" target="_blank">
+						<Fa icon={['fab', 'instagram']} iconSize="2x" />
+					</a>
+				</li>
+				<li>
+					<a href="https://linkedin.com" target="_blank">
+						<Fa icon={['fab', 'linkedin']} iconSize="2x" />
+					</a>
+				</li>
+			</ul>
+		</section>
+		<section class="flex items-center w-300">
+			<p>© 2021 teamGather. All rights reserved.</p>
+		</section>
+	</div>
+</footer>
+
+<style>
+	nav > ul::after {
+		content: '';
+		width: 100%;
+		height: 2px;
+		background-color: rgb(229 231 235 / var(--tw-border-opacity));
+		margin: 20px 0;
+	}
+
+	.w-300 {
+		max-width: 300px;
+		width: 100%;
+	}
+	@media (max-width: 768px) {
+		.social-links {
+			width: 80%;
+		}
+	}
+</style>
--- src/lib/pages/layout/index.ts
+++ src/lib/pages/layout/index.ts
@@ -1,1 +1,2 @@
 export { default as Header } from './header/Header.svelte';
+export { default as Footer } from './footer/Footer.svelte';
