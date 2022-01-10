4537066f80dd3cbc345605a176fb441d7d29ec46
Julestblt <jules.thibault2002@gmail.com>
2022-01-10 20:05:28 +0100

    ✨ add <CurrencySelector>

---
+++ src/lib/components/selectors/CurrencySelector.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Modal } from '$lib/components/modal';
+
+	let closeModal: boolean = false;
+</script>
+
+<Modal
+	buttonText="Currency"
+	modalTitle="Change currency"
+	buttonIcon={['fas', 'euro-sign']}
+	iconPosition="left"
+	iconSize="sm"
+	{closeModal}
+/>
---
+++ src/lib/components/selectors/index.ts
@@ -0,0 +1,2 @@
+export { default as CurrencySelector } from './CurrencySelector.svelte';
+export { default as LanguageSelector } from './LanguageSelector.svelte';
