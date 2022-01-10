2a95d5046342d53483b81d5edaf80050aa45b5b9
Julestblt <jules.thibault2002@gmail.com>
2022-01-10 18:28:24 +0100

    ✨ add <ModalContent>

---
+++ src/lib/components/modal/ModalContent.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	/**
+	 * Title of the modal.
+	 */
+	export let title: string = '';
+</script>
+
+<div
+	class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+>
+	<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+		<div class="sm:flex sm:items-start w-full">
+			<div class="mt-3 text-center sm:mt-0 sm:text-left w-full">
+				<h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+					{title}
+				</h3>
+				<div class="mt-2">
+					<slot name="modalContent" />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
