e52d339ce5dcb504231a07aaef5357d0cc77ed8b
Jules THIBAULT <jules.thibault2002@gmail.com>
2022-01-09 16:11:04 -0800

    ✨ add <MenuToggler>

---
+++ src/lib/pages/layout/header/menu-toggler/MenuToggler.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Fa from '$lib/components/fontawesome/Fa.svelte';
+</script>
+
+<section class="block md:hidden">
+	<button on:click>
+		<Fa icon={['fas', 'bars']} iconColor="text-gray-400" />
+	</button>
+</section>
