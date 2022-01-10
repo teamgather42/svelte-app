7fe1c0fe6f14efeb4341edbbf789023cf496ac0b
Jules THIBAULT <jules.thibault2002@gmail.com>
2022-01-09 16:09:42 -0800

    ✨ add <Logo>

---
+++ src/lib/components/logo/Logo.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	/**
+	 * Make the logo a lkink which redirect to homepage.
+	 */
+	export let isLink: boolean = true;
+	/**
+	 * Class for the logo.
+	 */
+	let className: string = '';
+	export { className as class };
+</script>
+
+<div>
+	{#if isLink}
+		<a href="/"><p class={className}>teamGather.</p></a>
+	{:else}
+		<p class={className}>teamGather.</p>
+	{/if}
+</div>
