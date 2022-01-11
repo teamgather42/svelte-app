7e1d320524e74e68f4f31e4ec06669c81ac81194
Julestblt <jules.thibault2002@gmail.com>
2022-01-11 13:25:50 +0100

    ✨ add validator.ts

---
+++ src/lib/pages/auth/validator.ts
@@ -0,0 +1,6 @@
+export const checkEmail = (email: string): boolean => {
+	const regex = new RegExp(
+		"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+	);
+	return regex.test(email);
+};
