30eb4b9fd7007e2eb9cd37b63fe0c006e7237df6
Jules THIBAULT <jules.thibault2002@gmail.com>
2022-01-10 16:11:58 -0800

    ✨ add new property to <ModalContent>

--- src/lib/components/modal/ModalContent.svelte
+++ src/lib/components/modal/ModalContent.svelte
@@ -3,6 +3,10 @@
 	 * Title of the modal.
 	 */
 	export let title: string = '';
+	/**
+	 * Additional class for the title.
+	 */
+	export let titleClass: string = '';
 </script>
 
 <div
@@ -11,7 +15,7 @@
 	<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
 		<div class="sm:flex sm:items-start w-full">
 			<div class="mt-3 text-center sm:mt-0 sm:text-left w-full">
-				<h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+				<h3 class={`text-lg leading-6 font-medium text-gray-900 ${titleClass}`}>
 					{title}
 				</h3>
 				<div class="mt-2">
