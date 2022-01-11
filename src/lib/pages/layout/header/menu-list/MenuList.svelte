6fcf7dda8f2571edcc27ec03622890c5d3ba49e2
Jules THIBAULT <jules.thibault2002@gmail.com>
2022-01-10 16:12:59 -0800

    ✨ add <MenuList>

--- src/lib/pages/layout/header/menu-list/MenuList.svelte
+++ src/lib/pages/layout/header/menu-list/MenuList.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
-	type Option = {
-		href: string;
-		label: string;
-	};
+	import type { Option } from '../nav.links';
+	import { createEventDispatcher } from 'svelte';
 
+	const dispatch = createEventDispatcher();
+
+	/**
+	 * Text that is displayed as heading.
+	 */
 	export let heading: string = '';
+	/**
+	 * List options.
+	 */
 	export let options: Option[] = null;
+
+	const linkHandler = () => dispatch('linkHandler');
 </script>
 
 <section>
-	<h2 class="border-b text-xl">{heading}</h2>
+	{#if heading}
+		<h2 class="border-b text-xl">{heading}</h2>
+	{/if}
 	<ul>
 		{#each options as { label, href }}
-			<li class="my-2 hover:bg-red-500"><a class="block py-2" {href}>{label}</a></li>
+			<li class="my-1 hover:bg-gray-200">
+				<a on:click={linkHandler} class="block py-1 pl-2" {href}>{label}</a>
+			</li>
 		{/each}
 	</ul>
 </section>
