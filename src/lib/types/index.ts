f39ca58f41a1b0282e1064b27a6819f093a21df5
Julestblt <jules.thibault2002@gmail.com>
2022-01-11 13:19:04 +0100

    🏷️ add localstorage.types.ts

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,3 +1,4 @@
 export * from './tailwind.types';
 export * from './fontawesome.types';
 export * from './base.types';
+export * from './localstorage.types';
---
+++ src/lib/types/localstorage.types.ts
@@ -0,0 +1,1 @@
+export type AuthenticatedToken = string | null;
