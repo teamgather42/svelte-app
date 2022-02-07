95b6d19f29002d73bf4064156f59bcd0824bfc62
Julestblt <jules.thibault2002@gmail.com>
2022-02-07 16:07:36 +0100

    📝 add doc to auth.store

--- src/lib/stores/auth.store.ts
+++ src/lib/stores/auth.store.ts
@@ -3,14 +3,26 @@ import type { AuthenticatedToken } from '$lib/types/';
 import { browser } from '$app/env';
 import { setLocalstorageItem, removeLocalstorageItem } from '$lib/utils/localstorage';
 
+/**
+ * Rertrieve the user token from the local storage.
+ */
 const token: AuthenticatedToken = browser && localStorage.getItem('token');
-
+/**
+ * Store that contain the user token if the user is logged in.
+ */
 const authenticatedToken = writable<AuthenticatedToken>(token);
-
+/**
+ * Allow to set the user's token.
+ * @param token The token.
+ */
 const setAuthenticatedToken = (token: string) => authenticatedToken.set(token);
-
+/**
+ * Remove the token from the store.
+ */
 const removeAuthenticatedToken = () => authenticatedToken.set(null);
-
+/**
+ * Update the local storage each time the token is update.
+ */
 authenticatedToken.subscribe((value) => {
 	if (value) setLocalstorageItem('token', value);
 	else if (value === null) removeLocalstorageItem('token');
