f1484eab751d7c61ebf58598afbc74786f23898e
Julestblt <jules.thibault2002@gmail.com>
2022-02-09 11:29:45 +0100

    🐛 fix test not passing

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,3 +2,9 @@ import { initI18n } from './src/lib/i18n';
 
 // Init i18n before executing scripts
 initI18n();
+
+jest.mock('$lib/utils/localstorage.ts', () => ({
+	localStorage: jest.fn(),
+	getLocalstorageItem: jest.fn(),
+	setLocalstorageItem: jest.fn()
+}));
