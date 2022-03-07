eb1a01b47bd1232a282a39d1364bb8570399a739
Julestblt <jules.thibault2002@gmail.com>
2022-03-07 11:13:55 +0100

    💄 update <ModalContainer>

--- src/lib/components/modal/ModalContainer.svelte
+++ src/lib/components/modal/ModalContainer.svelte
@@ -40,7 +40,7 @@
 		bind:this={container}
 		on:keydown={keyboardHandler}
 		on:click={closeModalHandler}
-		class="fixed z-10 inset-0 overflow-y-auto"
+		class="fixed z-50 inset-0 overflow-y-auto"
 		aria-labelledby="modal-title"
 		role="dialog"
 		aria-modal="true"
